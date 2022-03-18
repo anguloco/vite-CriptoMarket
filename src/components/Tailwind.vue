1d7716591e7b408afe959385c92380352b377ad3
Gerardo <82732062+anguloco@users.noreply.github.com>
2022-03-17 19:39:00 -0700

    cardqueloque
    reusable component for anyproject

--- src/components/Tailwind.vue
+++ src/components/Tailwind.vue
@@ -1,5 +1,4 @@
 <template>
-
   <!-- CARD CONTAINER -->
   <!-- AQUI ABAJO -->
   <div class="ml-4 contenedor-carta">
@@ -14,8 +13,7 @@
       "
     >
       <a href="#">
-      
-      <!--  <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" /> --> Do not need image for card
+        <!-- <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />  -->
       </a>
       <div class="p-5">
         <a href="#">
@@ -29,48 +27,120 @@
               dark:text-white
             "
           >
-            Noteworthy technology acquisitions 2021
+            Hey there 👋
           </h5>
         </a>
+        <img src="" alt="rounded image" />
+
+    
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          I’m Gerardo Max - a full-stack developer, and the creator of this web
+          app. I love all things related to front-end development and Deeply
+          interested in the Software Development aspects and functionality of
+          cloud computing.
         </p>
-        <a
-          href="#"
-          class="
-            inline-flex
-            items-center
-            py-2
-            px-3
-            text-sm
-            font-medium
-            text-center text-white
-            bg-blue-700
-            rounded-lg
-            hover:bg-blue-800
-            focus:ring-4 focus:ring-blue-300
-            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
-          "
-        >
-          {{ message }}
-          <svg
-            class="ml-2 -mr-1 w-4 h-4"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+        <div class="mt-4 sm:mt-0 sm:ml-3 group">
+          
+
+          <a href="https://angulo.co">
+            <button
+            href="#"
+            class="
+              w-full
+              space-x-2
+              items-center
+              justify-center
+              text-white
+              font-medium
+              bg-gradient-to-r
+              from-purple-700
+              to-pink-600
+              rounded-lg
+              shadow-sm
+              group-hover:shadow-lg
+              py-3
+              px-5
+              transform
+              group-hover:-translate-y-0.5
+              transition-all
+              duration-150
+              flex
+            "
           >
-            <path
-              fill-rule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+              ></path></svg
+            ><span>Check my work</span>
+            
+          </button>
+          </a>
+        </div>
+        
       </div>
+      <div>
+    <a
+      class="m-7 text-[#CF4BAD] underline italics hover:text-pink-700"
+      href="https://gerar.dev/"
+      target="_blank"
+      rel="noreferrer"
+      >Learn more about me</a
+    >
+  </div>
     </div>
   </div>
+
+  <a
+    href="#"
+    class="
+      inline-flex
+      items-center
+      py-2
+      px-3
+      text-sm
+      font-medium
+      text-center text-white
+      bg-blue-700
+      rounded-lg
+      hover:bg-blue-800
+      focus:ring-4 focus:ring-blue-300
+      dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+    "
+  >
+    <svg
+      class="ml-2 -mr-1 w-4 h-4"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  </a>
 </template>
+
+
+<style>
+.flex-wrap {
+  display: flex;
+  justify-content: space-between;
+  gap: 20rem;
+  flex: 1;
+  flex-wrap: wrap-reverse;
+}
+</style>
 
 <script>
 export default {};
